cbff047227304fe53b143cdb6694e653d9cfbb87
Daniel0224 <102885530+Daniel-0224@users.noreply.github.com>
2023-10-01 18:05:42 +0800

    Update hw1.c

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -1,5 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
+
+uint64_t x0 = 0x0000000000000001;
+uint64_t x1 = 0x8000000000000000;
+uint64_t x2 = 0x0012345678057800;
+
+
 uint16_t count_leading_zeros(uint64_t x)
 {
     x |= (x >> 1);
@@ -30,9 +36,9 @@ int count_trailing_zeros(uint64_t x)
 
 int main()
 {
-    uint64_t x0 = 0x8000000000000011;
-  
     printf("%d\n",count_trailing_zeros(x0));
+    printf("%d\n",count_trailing_zeros(x1));
+    printf("%d\n",count_trailing_zeros(x2));
     
     return 0;
 }
